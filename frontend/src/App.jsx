56402e0b48997a97e49adfcf7b7499d185790cde
Fathima misna <misnafasil01@gmail.com>
2026-04-30 20:54:28 +0530

    fix: connect frontend to live backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   const handleSubmit = async () => {
     try {
-      const res = await axios.post("http://127.0.0.1:5000/predict", {
+      const res = await axios.post("https://movie-ml-project.onrender.com/predict", {
         genre,
         mood,
         time
